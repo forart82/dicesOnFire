bb5b851b06c7f2f9ad2c629388fc8189a5fabc3f
forart82 <52891161+forart82@users.noreply.github.com>
2026-01-23 08:32:50 +0100

    Update ConfigManager.h

--- header/ConfigManager.h
+++ header/ConfigManager.h
@@ -8,28 +8,64 @@
 namespace config
 {
 
-  struct GUI_ELEMENT
+  struct Rectangle
   {
     sf::Vector2f position;
     sf::Vector2f size;
-    sf::Color color;
+    sf::Color innerColor;
+    sf::Color outerColor;
+    int thickness;
   };
 
-  inline std::map<std::string, GUI_ELEMENT> m_gui;
-  inline std::string m_fileName;
-  inline const GUI_ELEMENT DEFAULT_BUTTON = {
-      {0.f, 0.f},
-      {0.f, 0.f},
-      sf::Color::Red};
-
-  inline GUI_ELEMENT getKey(std::string key)
+  struct Circle 
   {
-    auto it = m_gui.find(key);
-    if (it != m_gui.end())
+    sf:: Vector2F position;
+    float radius;
+    sf::Color innerColor;
+    sf::Color outerColor;
+    int thickness;
+  }
+  
+  inline std::string m_fileName;
+
+  inline std::map<std::string, Rectangle> m_rectangle;
+  inline std::map<std::string, Circle> m_circle;
+ 
+  inline std:: m_form = "";
+
+  inline const Rectangle DEFAULT_RECTANGLE = {
+      {0.f, 0.f},
+      {0.f, 0.f},
+      sf::Color::Red,
+      sf::Color::Black,
+      2
+  }
+  inline const Circle DEFAULT_CIRCLE = {
+      {0.f, 0.f},
+      0,
+      sf::Color::Red,
+      sf::Color::Black,
+      2
+  }
+
+  inline Rectangle getRectangle(std::string key)
+  {
+    auto it = m_rectangle.find(key);
+    if (it != m_rectangle.end())
     {
       return it->second;
     }
-    return DEFAULT_BUTTON;
+    return DEFAULT_RECTANGLE;
+  }
+
+  inline Circle getCircle(std::string key)
+  {
+    auto it = m_circle.find(key);
+    if (it != m_circle.end())
+    {
+      return it->second;
+    }
+    return DEFAULT_CIRCLE;
   }
 
   inline bool load(std::string fileName)
@@ -46,32 +82,81 @@ namespace config
     while (std::getline(file, line))
     {
       std::stringstream ss(line);
-      std::string key, x_str, y_str, r_str, g_str, b_str, a_str;
-      std::getline(ss, key, ';');
-      std::getline(ss, x_str, ';');
-      std::getline(ss, y_str, ';');
-      std::getline(ss, r_str, ';');
-      std::getline(ss, g_str, ';');
-      std::getline(ss, b_str, ';');
-      std::getline(ss, a_str);
+      std::string key = std::getline(ss, key, ';');
 
-      if (!key.empty() && !x_str.empty() && !y_str.empty() && !r_str.empty() && !g_str.empty() && !b_str.empty() && !a_str.empty())
-      {
-        float x = std::stof(x_str);
-        float y = std::stof(y_str);
-        int r = std::stof(r_str);
-        int g = std::stof(g_str);
-        int b = std::stof(b_str);
-        int a = std::stof(a_str);
-        m_gui[key] = {
-                         sf::Vector2f(x, y),
-                         sf::Vector2f} std::cout
-                     << key << " " << x << " " << y << std::endl;
+      if(m_form=="RECTANGLE"){
+        makeRectangle(key, ss);
       }
+
+      m_form=key;
     }
 
     file.close();
     return true;
+  }
+
+  inline makeRectangle(
+    std::string key, 
+    std::stringstream ss)
+  {
+      std::string 
+        x_str, 
+        y_str, 
+        width_str,
+        height_str,
+        r1_str, 
+        g1_str, 
+        b1_str, 
+        a1_str,
+        r2_str, 
+        g2_str, 
+        b2_str, 
+        a2_str,
+        thickness_str;
+       
+      std::getline(ss, x_str, ';');
+      std::getline(ss, y_str, ';');
+      std::getline(ss, width_str, ';');
+      std::getline(ss, height_str, ';');
+      std::getline(ss, r1_str, ';');
+      std::getline(ss, g1_str, ';');
+      std::getline(ss, b1_str, ';');
+      std::getline(ss, a1_str, ';');
+      std::getline(ss, r2_str, ';');
+      std::getline(ss, g2_str, ';');
+      std::getline(ss, b2_str, ';');
+      std::getline(ss, a2_str, ';');
+      std::getline(ss, thickness_str, ';');
+ 
+
+      if ( 
+         !key.empty() && 
+         !x_str.empty() && 
+         !y_str.empty() &&
+         !widthv_str.empty() &&
+         !r_str.empty() && 
+         !g_str.empty() && 
+         !b_str.empty() && 
+         !a_str.empty() &&
+         !r_str.empty() &&
+         !r_str.empty() &&
+         !r_str.empty() &&
+         !r_str.empty() &&
+         !r_str.empty())
+      {
+        float x = std::stof(x_str);
+        float y = std::stof(y_str);
+        float width = std::stof(width_str);
+        float height std::stof(height_dtr);
+        int r = std::stof(r_str);
+        int g = std::stof(g_str);
+        int b = std::stof(b_str);
+        int a = std::stof(a_str);
+        m_rectangle[key] = {
+          sf::Vector2f(x, y),
+          sf::Vector2f(width, height),
+          
+
   }
 
   inline void reload()
